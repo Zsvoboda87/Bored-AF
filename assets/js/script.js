ee394599d3653698cc3bca7ac8df354925455fbd
Zsvob <svobodazachary@gmail.com>
2022-01-06 12:34:26 -0500

    add genre modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+var searchMoviesBtn = document.querySelector("#search-movies");
+
+var modalBg = document.querySelector(".modal-bg");
+
+
+searchMoviesBtn.addEventListener("click", function(){
+    modalBg.classList.add("bg-active")
+    console.log("click")
+})
