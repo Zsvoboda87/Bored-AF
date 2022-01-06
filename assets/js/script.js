2d0219efd2d68a0d04f40023c6a9d100cfe6fa50
Zsvob <svobodazachary@gmail.com>
2022-01-06 12:46:35 -0500

    fetch IMDB API data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,45 @@
 $(document).foundation();
 
 var searchMoviesBtn = document.querySelector("#search-movies");
-
+var runGenreSearch= document.querySelector("#search-genre");
 var modalBg = document.querySelector(".modal-bg");
+var checkboxes = document.querySelectorAll(".checkbox");
+
+var str = "";
 
 
 searchMoviesBtn.addEventListener("click", function(){
     modalBg.classList.add("bg-active")
-    console.log("click")
 })
+
+runGenreSearch.addEventListener('click', function(){
+    modalBg.classList.remove("bg-active");
+    for ( i = 0; i < checkboxes.length; i++)  {
+        if ( checkboxes[i].checked === true ){
+            str += checkboxes[i].value + ","
+        }
+    };
+    movieAPI (str);
+    resetCheckboxes();
+})
+
+var movieAPI = function (genre) {
+    var apiUrl = "https://imdb-api.com/API/AdvancedSearch/k_0m8x9src/?genres=" + genre + "&count=10"
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data)
+        
+        })} else {
+            window.alert('not valid')
+        }
+    });
+};
+
+var resetCheckboxes = function() {
+    str = "";
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+}
+
