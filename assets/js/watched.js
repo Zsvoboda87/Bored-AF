ccb92c4b1b5c2989829b32660b7d00b6b40b9093
Brina <sabrinazanin97@gmail.com>
2022-01-08 13:48:09 -0500

    added display of movies to watched movies

--- assets/js/watched.js
+++ assets/js/watched.js
@@ -1,9 +1,10 @@
-var watchedmv = localStorage.getItem("watched");
+var watchedmv = JSON.parse(localStorage.getItem("watched"));
+var previousmovies = document.querySelector("#wmovies");
 
 var checklocal = function() {
     if (localStorage.getItem("watched")) {
         console.log(watchedmv);
-        for (i=0; i<2; i++) {
+        for (i=0; i<watchedmv.length; i++) {
             var mTitle = watchedmv[i].title;
             var mImageURL = watchedmv[i].image;
             displaywatched(mTitle,mImageURL);
@@ -16,17 +17,19 @@ var checklocal = function() {
      console.log(mTitle);
      console.log(mImageURL);
     
-//     var movieCard = document.createElement("div")
+    var movieCard = document.createElement("div")
 
-//     var movTitleEl = document.createElement("h4");
-//     movTitleEl.setAttribute("data-mTitle", mTitle)
-//     movTitleEl.textContent = mTitle;
-//     movieCard.appendChild(movTitleEl);
+    var movTitleEl = document.createElement("h4");
+    movTitleEl.setAttribute("data-mTitle", mTitle)
+    movTitleEl.textContent = mTitle;
+    movieCard.appendChild(movTitleEl);
 
-//     var movImageEl = document.createElement("img");
-//     movImageEl.src = mImageURL
-//     movImageEl.classList.add("mov-image-width")
-//     movieCard.appendChild(movImageEl);
+    var movImageEl = document.createElement("img");
+    movImageEl.src = mImageURL
+    movImageEl.classList.add("mov-image-width")
+    movieCard.appendChild(movImageEl);
+
+    previousmovies.appendChild(movieCard);
 }
 
 checklocal();
